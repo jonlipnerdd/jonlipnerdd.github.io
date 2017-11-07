b43fa8bfef68ff3ae527965ff84038057abb0be9
jonlipner <jonatanlipner@gmail.com>
2017-11-07 17:51:25 -0300

    missing texts

--- js/select.js
+++ js/select.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$('.button').on('click', function(){
   		var projectSelected = $(this).data("project");
+  		$('.img').fadeOut(1000);
   		$('.button, .img').removeClass('selected');
    		$(this).addClass('selected');
     	$(".img[data-project='" + projectSelected + "']").addClass("selected");
+    	$(".img[data-project='" + projectSelected + "']").fadeIn(600);
 	});
 });
